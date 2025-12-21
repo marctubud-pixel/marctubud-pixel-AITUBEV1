a76b3151b291fa153c50a17c423e6bd29ba35408
marctubud-pixel <marctubud@gmail.com>
2025-12-21 15:49:52 +0800

    Delete app/test-page directory

--- app/test-page/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-    return (
-      <div style={{ color: 'white', padding: '50px' }}>
-        <h1>这是测试页面</h1>
-        <p>如果你能看到这句话，说明路由是好的！</p>
-      </div>
-    );
-  }
-  
