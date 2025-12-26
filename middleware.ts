1e936c02c641545b35461f2d26e544d96dcf43bc
marctubud-pixel <marctubud@gmail.com>
2025-12-26 22:18:26 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,16 @@
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/utils/supabase/middleware'
 
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
+}
+
+export const config = {
+  matcher: [
+    /*
+     * 匹配所有路径，除了静态资源
+     * (确保每一页的请求都会经过中间件检查身份)
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
